488eb28558c6a6a9dce939cd48cd6178d6b28960
Eben <me@ebenroux.co.za>
2016-01-01 08:39:27 +0200

    - v-4.0.3

--- Shuttle.ESB.Tests/Properties/AssemblyInfo.cs
+++ Shuttle.ESB.Tests/Properties/AssemblyInfo.cs
@@ -25,11 +25,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Shuttle.ESB.Tests for .NET Framework 4.6.1")]
 #endif
 
-[assembly: AssemblyVersion("4.0.2.0")]
+[assembly: AssemblyVersion("4.0.3.0")]
 [assembly: AssemblyCopyright("Copyright © Eben Roux 2010-2015")]
 [assembly: AssemblyProduct("Shuttle.ESB")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("4.0.2")]
+[assembly: AssemblyInformationalVersion("4.0.3")]
 [assembly: ComVisible(false)]
 
